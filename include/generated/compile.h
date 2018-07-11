982f3f072585a1f4c916016f22738d50096efc54
steward-fu <g9313716@yuntech.edu.tw>
2018-07-11 11:23:08 +0800

    added led_gpio driver(/drivers/led)

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,6 +1,6 @@
-/* This file is auto generated, version 4 */
+/* This file is auto generated, version 1 */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#4 Wed Jul 11 01:43:48 CST 2018"
+#define UTS_VERSION "#1 Wed Jul 11 10:54:04 CST 2018"
 #define LINUX_COMPILE_BY "steward"
 #define LINUX_COMPILE_HOST "debian"
 #define LINUX_COMPILER "gcc version 7.2.1 20171011 (Linaro GCC 7.2-2017.11)"
